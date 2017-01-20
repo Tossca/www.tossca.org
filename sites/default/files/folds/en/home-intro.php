1e119f340f911ac8cff8d1e863ec17dacee443a8
Stefan Bodewig <stefan.bodewig@innoq.com>
2017-01-20 16:59:45 +0100

    protected seems a bit over the top

--- sites/default/files/folds/en/home-intro.php
+++ sites/default/files/folds/en/home-intro.php
@@ -1,6 +1,6 @@
 <div class="fold--intro fold--intro__front editor--content editor--content__consider-margins">
   <h1 class="font-size__xxxxl base--clearfix">
-    <div class="base--clearfix">Open Source Legally Protected.</div>
+    <div class="base--clearfix">Open Source Legally Safe.</div>
     <div class="fold--intro-signature-prefix">With just one </div><div class="fold--intro-signature">Signature</div>
   </h1>
   <p class="font-size__xl">
